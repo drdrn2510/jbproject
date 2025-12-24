a9f75d399da9f96438113f5604e5d5f410a78b32
Doron Hacham <Doron@amital.co.il>
2025-12-24 14:07:51 +0200

    readme

--- infra_simulator.py
+++ infra_simulator.py
@@ -6,12 +6,16 @@ from userinput import create_machine,saveasjson,run_install_script
 
 
 
-
+#create a new machine by user input
 newvm =create_machine()
 print(f"Created machine: {newvm.name}")
 
+#save the new machine as json file
 saveasjson(newvm)
+
+#run install script on the new machine
 run_install_script()
+
 
 
 
